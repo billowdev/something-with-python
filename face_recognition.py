c408af37cbe517dc1fed7032a28154c696b63833
Akkarapon Phikulsri <78582854+lacakp@users.noreply.github.com>
2021-09-21 15:37:13 +0700

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -12,17 +12,11 @@ import os
 import cv2
 import numpy as np
 
-def face_detection(frame):
-	# to gray
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	# detect faces
-	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+def face_detection(faces, gray, img):
 	for (x, y, w, h) in faces:
-		img = frame[y-10:y+h+10, x-10:x+w+10][:,:,::-1]
-		
-		dets = cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+		# img = frame[y-10:y+h+10, x-10:x+w+10][:,:,::-1]
+		img = cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
 		# พื้นที่ใบหน้า
-		
 		g = gray[y:y+h, x:x+h]
 		c = img[y:y+h, x:x+h]
 
@@ -34,32 +28,40 @@ def face_detection(frame):
 			# วาดสี่เหลี่ยม
 			cv2.rectangle(c, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
 	
-	cv2.imshow('img', img)
-	
+# https://stackoverflow.com/questions/918154/relative-paths-in-python
+"""
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'relative/path/to/file/you/want')
 
-if __name__ == "__main__":
-	# https://stackoverflow.com/questions/918154/relative-paths-in-python
-	"""
-	import os
-	dirname = os.path.dirname(__file__)
-	filename = os.path.join(dirname, 'relative/path/to/file/you/want')
+"""
+# --- เตรียม haarcascade eye และ face --- #
+dirname = os.path.dirname(__file__)
+h_eye = os.path.join(dirname, 'haarcascade_eye.xml')
+h_face = os.path.join(dirname, 'haarcascade_frontalface_default.xml')
 
-	"""
-	# --- เตรียม haarcascade eye และ face --- #
-	dirname = os.path.dirname(__file__)
-	h_eye = os.path.join(dirname, 'haarcascade_eye.xml')
-	h_face = os.path.join(dirname, 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(h_eye)
+face_cascade = cv2.CascadeClassifier(h_face)
+#read image
+img = cv2.imread('Untitled-1.jpg')
+#convert to gray
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#detect faces
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-	eye_cascade = cv2.CascadeClassifier(h_eye)
-	face_cascade = cv2.CascadeClassifier(h_face)
+face_detection(faces, gray, img)
 
-	# ใช้กล้อง
-	cap = cv2.VideoCapture(0)
+cv2.imshow('img',img) #shows image
+cv2.waitKey(0) #waits until a key is pressed to progress
+cv2.destroyAllWindows() #closes windows
 
-	while True:
-		_, frame = cap.read()
-		face_detection(frame)
+# ใช้กล้อง
+# cap = cv2.VideoCapture(0)
 
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+# while True:
+# 	_, frame = cap.read()
+# 	face_detection(frame)
+
+# 	if cv2.waitKey(1) & 0xFF == ord('q'):
+# 		break
 
